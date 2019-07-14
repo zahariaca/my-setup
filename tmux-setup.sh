00aa47d820a08fce0258ae738e99079556e0d85c
Alexandru Zaharia <zaharia.c.alexandru@gmail.com>
2019-07-14 20:23:48 +0300

    Initial tmux conf

---
+++ tmux-setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+#install helper software
+sudo apt-get install vim make gcc
+
+# setup vim as system editor
+echo "export $EDITOR='vim' >> ~/.bashrc"
+
+# install ruby
+cd $HOME
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.3.1
+rbenv global 2.3.1
+ruby -v
+
+# make directory for git
+mkdir ~/git
+pusdh ~/git
+
+# clone tmux and tmuxinator
+
+git clone https://github.com/gpakosz/.tmux.git
+
+# link .tmux.conf from repo to ~/.tmux.conf
+ln -s -f ~/git/.tmux/.tmux.conf   ~/.tmux.conf
+
+# copy local editable conf from repo to home
+cp ~/git/.tmux/.tmux.conf.local ~/.tmux.conf.local
+
+# install tmuxinator
+gem install tmuxinator
